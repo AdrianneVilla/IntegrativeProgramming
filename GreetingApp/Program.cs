4e1a08a31490fa2948ee8d957ea94f3655aeed8e
villa.286986 <villa.286986@LAB-A-PC36>
2024-02-08 08:01:52 +0800

    Add project files.

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GreetingApp
+{
+    internal class Program
+    {
+        static void GreetingProgram(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+
+            Console.Write("Enter the total number of your enrolled courses: ");
+            int enrolled;
+            enrolled = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the price of your favorite book: ");
+            double price;
+            price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nName: " + name);
+            Console.WriteLine("Total enrolled courses: " + enrolled);
+            Console.WriteLine("Price of my favorite book: " + price);
+        }
+    }
+}
